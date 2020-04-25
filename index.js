e05ec0523f787f5c69457d4a85f028541a769266
David <david.a.25897@gmail.com>
2020-04-25 20:01:49 +0530

    Removed options and all

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.options("/*", function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.sendStatus(200);
-});
-
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
