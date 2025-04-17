47a5c3e5f35b179cdf62e8ee65c6906c8f0332c6
David <david.a.25897@gmail.com>
2025-04-17 23:13:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express();
 const bodyParser=require('body-parser');
 const mongoClient=require('mongodb');
 const cors=require('cors');
-const url='mongodb+srv://david:david@1997@cluster0-o20vy.mongodb.net/MovieDB?retryWrites=true&w=majority';
+const url = 'mongodb+srv://david:david%401997@cluster0-o20vy.mongodb.net/MovieDB?retryWrites=true&w=majority';
 app.use(cors());
 app.use(bodyParser.json());
 
